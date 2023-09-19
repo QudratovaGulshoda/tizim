dd99ac0753ac9db48b186a61092101fbbc38fdd4
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-09-19 23:31:42 +0500

    test

--- config/settings.py
+++ config/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-_gvc6646qaj4#%yf3120od_u66x(wu%hg1)nz9z6$4tyqq(u$h
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -42,7 +40,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'authentication.apps.AuthenticationConfig',
     ############# Packages #################
-    "corsheaders", 
+    "corsheaders",
     'rest_framework',
     'rest_framework.authtoken',
     # 'drf_yasg',
@@ -62,7 +60,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 CORS_ALLOWED_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
@@ -74,9 +71,7 @@ CORS_ALLOW_METHODS = [
     "GET",
 ]
 
-
 ROOT_URLCONF = 'config.urls'
-
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
@@ -90,7 +85,7 @@ CSRF_TRUSTED_ORIGINS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +100,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -115,7 +109,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -135,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -147,10 +139,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-import os
+
 STATIC_URL = 'static/'
 if DEBUG:
     STATICFILES_DIRS = [
@@ -161,7 +152,6 @@ else:
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
